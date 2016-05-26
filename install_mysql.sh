e701cb87d7bdf0182a072f14e88df8ab57d9ad14
ganluchang <lch.gan@qq.com>
2016-05-26 09:43:20 +0800

    Update install_mysql.sh
    更新

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,16 +1,21 @@
+
+
 #!/bin/bash
 set -u
 set -e
 set -x
 #create dir
 mysql_port=3306   #实例端口号
-mysql_tar_file=/opt/Percona-Server-5.7.11-4-Linux.x86_64.ssl101.tar #MySQL二级制包位置
+mysql_tar_file=/opt/Percona-Server-5.6.17-rel66.0-608.Linux.x86_64.tar.gz #MySQL二级制包位置
 mysql_base_dir=/usr/local/mysql  #软连接
 mysql_data_dir=/data/mysql${mysql_port} #数据文件路径
 
-mkdir -p /opt/mysql  #
-cd /opt/mysql
-tar -xvf /opt/${mysql_tar_file} 
+#mkdir -p /opt/mysql  #
+cd /opt/
+tar -xvf ${mysql_tar_file}
+#mv Percona-Server-5.6.17-rel66.0-608 mysql${port}
+cd /usr/local/
+ln -s /opt/Percona-Server-5.6.17-rel66.0-608.Linux.x86_64  mysql
 #check mysql user and group
 if [[ `id mysql` && `groups mysql` ]]; then
 echo "mysql user or group already exist!"
@@ -21,7 +26,6 @@ groupadd mysql
 useradd -r -g mysql mysql -s /sbin/nologin -d /home/mysql -m
 fi
 #create mysql data,log dir.
-mkdir -p /data/mysql/mysql${mysql_port}{data,logs,tmp}
-mkdir -p /data/mysql/mysql${mysql_prot}/logs/bin-log
-mkdir -p /data/mysql/mysql${mysql_prot}/logs/relay-log
-
+mkdir -p /data/mysql/mysql${mysql_port}/{data,logs,tmp}
+mkdir -p /data/mysql/mysql${mysql_port}/logs/bin-log
+mkdir -p /data/mysql/mysql${mysql_port}/logs/relay-log
